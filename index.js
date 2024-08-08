67bfe8ff41ebf10a741889d1bd2352843fe4c316
Aryan Jain <aj994722@gmail.com>
2024-08-08 17:33:59 +0530

    Final Commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-const path = require("path");
 
+const path = require("path");
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
 app.use(express.static(path.join(__dirname,"views")));
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+const { v4: uuidv4 } = require('uuid');
+
+let methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -18,11 +22,13 @@ app.listen(port, ()=>{
 let posts = [
 
     {
+        id:uuidv4(),
         username:"Aryan",
         content:"Hi there! I am in Sagar"
     },
 
     {
+        id:uuidv4(),
         username:"Pragya",
         content:"Hi there! I am in Bhopal"
     },
@@ -38,6 +44,33 @@ app.get("/posts/new",(req,res) => {
 
 app.post("/posts",(req,res) => {
     let {username,content} = req.body;
-    posts.push({username : username,content : content});
-    res.send("Post Request Working");
+    posts.push({id : uuidv4(), username : username,content : content});
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id",(req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id == p.id);
+    console.log(post);
+    res.render("show",{post});
+});
+
+app.get("/posts/edit/:id",(req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id == p.id);
+    res.render("edit",{post});
+});
+
+app.patch("/posts/:id",(req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id == p.id);
+    let newcontent = req.body.newcontent;
+    post.content = newcontent;
+    res.redirect("/posts");
+});
+
+app.delete("/posts/:id",(req,res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id != p.id);
+    res.redirect("/posts");
 });
